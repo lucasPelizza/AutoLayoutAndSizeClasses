c1069eff6c65e081e1ea7efc65b2458c33f5eafd
LucasPeli <pelizza.lucas@gmail.com>
2017-09-07 14:57:13 -0300

    Add a new screen which uses constraints and size classes
    * Add multiple examples
    * Add a new screen with size classes and constraints

---
+++ AutoLayoutAndSizeClasses/ConstraintsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ConstraintsViewController.swift
+//  AutoLayoutAndSizeClasses
+//
+//  Created by Lucas Pelizza on 9/6/17.
+//  Copyright © 2017 Lucas Pelizza. All rights reserved.
+//
+
+import UIKit
+
+class ConstraintsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
